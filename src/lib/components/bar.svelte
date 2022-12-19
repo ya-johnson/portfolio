49c4aede965a7861b62a56959cd3c29caad21ffc
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-19 16:33:34 +0200

    add blog(share) option to Bar component

--- src/lib/components/bar.svelte
+++ src/lib/components/bar.svelte
@@ -9,6 +9,13 @@
     <p class="bar-text">great idea can come from anywhere ~</p>
     <p class="bar-text">great idea can come from anywhere ~</p>
   </div>
+{:else if type === 'share'}
+<div class="bar bg-blue">
+  <p class="bar-text">sharing is caring ✽ sharing is caring ✽</p>
+  <p class="bar-text">sharing is caring ✽ sharing is caring ✽</p>
+  <p class="bar-text">sharing is caring ✽ sharing is caring ✽</p>
+  <p class="bar-text">sharing is caring ✽ sharing is caring ✽</p>
+</div>
 {:else if type === 'design'}
   <div class="bar bg-white">
     <p class="bar-text">design • design • design • design •</p>
@@ -32,17 +39,17 @@
     display: flex;
     align-items: center;
     padding: 12px 0;
-    margin: 40px 0;
     border: solid 2px var(--black);
   }
 
   .bar-text {
-    font-size: 10vh;
+    font-size: 4vw;
     font-weight: 700;
     line-height: 1.2;
     text-transform: uppercase;
     white-space: nowrap;
     margin-left: 20px;
+    margin-bottom: 0;
     animation: slide linear infinite 20s;
   }
 
@@ -53,6 +60,12 @@
 
     100% {
       transform: translateX(-100%);
+    }
+  }
+
+  @media screen and (max-width: 991px) {
+    .bar-text {
+      font-size: 4vh;
     }
   }
 
