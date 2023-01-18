eaa53e73c2049cafabc4af292871dd8c9caa5603
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-18 19:16:40 +0200

    add initial syntax highlight component

---
+++ src/lib/components/syntax.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Prism from 'prismjs'
+
+  export let language
+  export let source
+
+</script>
+
+
+<div class="code">
+  {@html Prism.highlight(source, Prism.languages[language])}
+</div>
