941dd4d28621f5cad0a0fa366ac7169c6b824c99
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-18 20:21:54 +0200

    update Syntax component - add filepath

--- src/lib/components/syntax.svelte
+++ src/lib/components/syntax.svelte
@@ -2,11 +2,12 @@
   import Prism from 'prismjs'
 
   export let language
+  export let filePath
   export let source
-
 </script>
 
-
 <div class="code">
+  <p class="file-path">{filePath}</p>
   {@html Prism.highlight(source, Prism.languages[language])}
 </div>
+
