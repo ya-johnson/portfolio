3a0525a7efca764d0e9b2c0847ca4a61b6d7bb30
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-02-05 13:34:12 +0200

    update Syntax component - from prismjs to svelte-highlight

--- src/lib/components/syntax.svelte
+++ src/lib/components/syntax.svelte
@@ -1,13 +1,24 @@
 <script>
-  import Prism from 'prismjs'
+  import Highlight from 'svelte-highlight'
+  import javascript from 'svelte-highlight/languages/javascript'
+  // import css from 'svelte-highlight/languages/css'
+  import materialDarker from 'svelte-highlight/styles/material-darker'
 
-  export let language
-  export let filePath
-  export let source
+  // export let language
+  // export let filepath
+  export let code
 </script>
 
-<div class="code">
-  <p class="file-path">{filePath}</p>
-  {@html Prism.highlight(source, Prism.languages[language])}
+<svelte:head>{@html materialDarker}</svelte:head>
+
+<div class='code'>
+  <Highlight language={javascript} {code} />
 </div>
 
+<style>
+  .code {
+    margin: 60px 0;
+    box-shadow: #6272a4 8px 8px 0 0;
+  }
+
+</style>
