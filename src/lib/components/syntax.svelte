60e236cd55eb93872d0a5446b655fcc68f0c1c01
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-02-14 10:41:19 +0200

    update Syntax component - add styles and filepath

--- src/lib/components/syntax.svelte
+++ src/lib/components/syntax.svelte
@@ -1,24 +1,35 @@
 <script>
   import Highlight from 'svelte-highlight'
   import javascript from 'svelte-highlight/languages/javascript'
-  // import css from 'svelte-highlight/languages/css'
-  import materialDarker from 'svelte-highlight/styles/material-darker'
+  import { snazzy } from 'svelte-highlight/styles'
 
-  // export let language
-  // export let filepath
+  export let filepath
   export let code
 </script>
 
-<svelte:head>{@html materialDarker}</svelte:head>
+<svelte:head>{@html snazzy}</svelte:head>
 
 <div class='code'>
+  <p>{filepath}</p>
   <Highlight language={javascript} {code} />
 </div>
 
 <style>
+  
   .code {
+    font-family: 'Source Code Pro', monospace;
+    padding: 12px 12px 12px 0;
     margin: 60px 0;
     box-shadow: #6272a4 8px 8px 0 0;
+    background-color: #282a36;
+  }
+
+  p {
+    color: #F9F9F9;
+    width: fit-content;
+    padding-bottom: 4px;
+    margin-left: 18px;
+    border-bottom: solid 2px #6272a4;
   }
 
 </style>
