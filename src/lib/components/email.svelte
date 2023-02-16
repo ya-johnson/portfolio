4480edbb56131f0cf2aa05304337de2a99c2ed12
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-02-16 09:44:10 +0200

    update Email component - add initial size option

--- src/lib/components/email.svelte
+++ src/lib/components/email.svelte
@@ -1,6 +1,7 @@
 <script>
 
   export let color
+  export let size
   let vi
 
   const copyEmail = () => {
@@ -14,11 +15,12 @@
 </script>
 
 <div class="box">
-  <div class="email {color}" on:click={copyEmail} on:keydown={copyEmail}>
+  <div class="email {color} {size}" on:click={copyEmail} on:keydown={copyEmail}>
     yonathan@yajohnson.xyz
     <div class="copy" bind:this={vi}></div>
   </div>
 </div>
+
 
 
 <style>
@@ -26,7 +28,6 @@
   .box {
     position: relative;
     width: 100%;
-    margin: 20px 0;
   }
 
   .pink,
@@ -47,40 +48,69 @@
     background-color: var(--yellow-darker);
   }
 
+  .normal {
+    font-size: 58px;
+    padding: 34px;
+    border-radius: 20px;
+    box-shadow: var(--black) 6px 6px 0 0;
+  }
+
+  .small {
+    font-size: 22px;
+    padding: 12px;
+    border-radius: 10px;
+    box-shadow: var(--black) 4px 4px 0 0;
+  }
+
+  .normal > .copy {
+    height: 38px;
+    width: 30px;
+    font-size: 22px;
+    margin-left: 34px;
+    border-radius: 8px;
+    box-shadow: var(--black) 5px 5px 0 0;
+  }
+
+  .small > .copy {
+    height: 24px;
+    width: 18px;
+    font-size: 14px;
+    margin-left: 14px;
+    border-radius: 4px;
+    box-shadow: var(--black) 3px 3px 0 0;
+  }
+
+  .normal:hover {
+    top: 6px;
+    left: calc(50% + 6px);
+    box-shadow: var(--black) 3px 3px 0 0;
+  }
+
+  .small:hover {
+    top: 4px;
+    left: calc(50% + 4px);
+    box-shadow: var(--black) 2px 2px 0 0;
+  }
+
   .email {
     cursor: pointer;
     position: absolute;
     top: 0;
     left: 50%;
     display: flex;
-    font-size: 58px;
+    align-items: center;
     font-weight: 700;
     text-align: center;
-    padding: 34px;
     border: 2px solid var(--black);
-    border-radius: 20px;
-    box-shadow: var(--black) 6px 6px 0 0;
     transform: translateX(-50%);
     transition: all ease-out .15s;
   }
 
-  .email:hover {
-    top: 6px;
-    left: calc(50% + 6px);
-    box-shadow: var(--black) 2px 2px 0 0;
-  }
-
   .copy {
     position: relative;
-    height: 38px;
-    width: 30px;
     display: flex;
     align-items: center;
-    font-size: 22px;
-    margin-left: 34px;
-    border-radius: 8px;
     border: solid 2px var(--black);
-    box-shadow: var(--black) 5px 5px 0 0;
     transition: all ease-out .15s;
   }
 
